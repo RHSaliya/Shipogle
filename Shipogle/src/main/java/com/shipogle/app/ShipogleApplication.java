e68b3459d1d50af706d8e0317dfa1b0259358af8
slakhanpal <sh475218@dal.ca>
2023-01-31 02:19:22 -0400

    Initial Project Structure : Shipogle

---
+++ Shipogle/src/main/java/com/shipogle/app/ShipogleApplication.java
@@ -0,0 +1,13 @@
+package com.shipogle.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipogleApplication.class, args);
+	}
+
+}
