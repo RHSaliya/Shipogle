181b6f5d8dc526452faa58a8a094fbd91c2a8adf
kadivar <kadivarnand007@gmail.com>
2023-02-04 12:59:43 -0400

    Fixed DB connection error

--- backend/src/main/java/com/shipogle/app/ShipogleApplication.java
+++ backend/src/main/java/com/shipogle/app/ShipogleApplication.java
@@ -2,12 +2,19 @@ package com.shipogle.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ShipogleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShipogleApplication.class, args);
 	}
 
+	@GetMapping("/")
+	String helloworld(){
+		return "Application running";
+	}
 }
