4dedd7093bae39425e22f6060dcf5e9c80b26c95
Almasfiza Anwar Hussain Shaikh <al723449@dal.ca>
2023-02-16 08:34:04 -0400

    Add Login page

--- shipogle-frontend/src/App.js
+++ shipogle-frontend/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
 import Registration from './pages/Registration';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration/>} />
       </Routes>
     </div>
--- shipogle-frontend/src/components/Header.js
+++ shipogle-frontend/src/components/Header.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Header(props) {
   return (
     <div className="Header">
-        <div>{props.title}</div>
-        <div>{props.info}</div>
+        <div className="title">{props.title}</div>
+        <div classname="info">{props.info}</div>
     </div>
     
   )
