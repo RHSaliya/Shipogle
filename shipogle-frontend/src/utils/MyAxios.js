b75cbca2ae3433b9832fd8ae2cd06908df17e9d8
RHSaliya <RHS.Dev@outlook.com>
2023-03-11 23:24:45 -0400

    inbox: Created API Interceptor.

--- shipogle-frontend/src/utils/MyAxios.js
+++ shipogle-frontend/src/utils/MyAxios.js
@@ -3,16 +3,10 @@ import Cookies from 'js-cookie';
 
 const customAxios = axios.create({
     timeout: 10000,
-    headers: {
-        Authorization: `Bearer ${Cookies.get('token')}`
-    }
 });
 
-// Step-2: Create request, response & error handlers
 const requestHandler = request => {
-    // Token will be dynamic so we can use any app-specific way to always   
-    // fetch the new token before making the call
-    request.headers.Authorization = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIwMTIzNDU2Nzg5IiwibmFtZSI6IlNhbXBsZSIsImlhdCI6MTUxNjIzODIzfQ.ZEBwz4pWYGqgFJc6DIi7HdTN0z5Pfs4Lcv4ZNwMr1rs';
+    request.headers.Authorization = `Bearer ${Cookies.get('authToken')}`;
 
     return request;
 };
@@ -29,9 +23,6 @@ const errorHandler = error => {
     return Promise.reject(error);
 };
 
-// Step-3: Configure/make use of request & response interceptors from Axios
-// Note: You can create one method say configureInterceptors, add below in that,
-// export and call it in an init function of the application/page.
 customAxios.interceptors.request.use(
     (request) => requestHandler(request),
     (error) => errorHandler(error)
@@ -42,6 +33,7 @@ customAxios.interceptors.response.use(
     (error) => errorHandler(error)
 );
 
-
-// Step-4: Export the newly created Axios instance to be used in different locations.
 export default customAxios;
+
+
+// Reference: https://blog.clairvoyantsoft.com/intercepting-requests-responses-using-axios-df498b6cab62
