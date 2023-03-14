e84edd5ce3c52a826eaddc30be350c504e7af1f2
RHSaliya <RHS.Dev@outlook.com>
2023-03-14 00:56:55 -0300

    inbox: fixed className in two files.

--- shipogle-frontend/src/pages/RegSuccessful.js
+++ shipogle-frontend/src/pages/RegSuccessful.js
@@ -1,28 +1,28 @@
 import React from 'react'
-import {Link, useLocation} from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Header from '../components/Header';
 import shipogleLogo from '../assets/shipogleLogo.png';
 export default function RegSuccessful() {
   const loc = useLocation();
   return (
-    <div classname="regSuccessPage">
-         <Header 
-      title="S H I P O G L E" 
-      info = "tagline" 
+    <div className="regSuccessPage">
+      <Header
+        title="S H I P O G L E"
+        info="tagline"
       />
       <center>
-      <img alt="logo" src={shipogleLogo} width="200px" height="200px">
+        <img alt="logo" src={shipogleLogo} width="200px" height="200px">
 
-      </img>
+        </img>
       </center>
       <div className="regSuccessBox">
         <h1>Registration successful!</h1>
         <Link to="/login"><p>Click here to login</p></Link>
-        
+
+
+      </div>
 
     </div>
-      
-    </div>
-   
+
   )
 }
