1823297f8c5af6e243c293bde522a969164578c8
Nandkumar Kadivar <nn521973@dal.ca>
2023-03-16 01:18:28 -0300

    Get all orders of sender api

--- backend/src/main/java/com/shipogle/app/controller/PackageOrderController.java
+++ backend/src/main/java/com/shipogle/app/controller/PackageOrderController.java
@@ -1,18 +1,23 @@
 package com.shipogle.app.controller;
 
+import com.shipogle.app.model.Order;
+import com.shipogle.app.model.PackageOrder;
 import com.shipogle.app.service.PackageOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
 public class PackageOrderController {
     @Autowired
     PackageOrderService packageOrderService;
+
+    @GetMapping("/package/order/getall")
+    public List<PackageOrder> getSenderOrders(){
+        return packageOrderService.getSenderOrders();
+    }
 
     @PutMapping("/package/order/cancel")
     public String cancelOrder(@RequestBody Map<String,String> req){
--- backend/src/main/java/com/shipogle/app/service/PackageOrderService.java
+++ backend/src/main/java/com/shipogle/app/service/PackageOrderService.java
@@ -2,16 +2,24 @@ package com.shipogle.app.service;
 
 import com.shipogle.app.model.PackageOrder;
 import com.shipogle.app.model.PackageRequest;
+import com.shipogle.app.model.User;
 import com.shipogle.app.repository.PackageOrderRepository;
+import com.shipogle.app.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Random;
 
 @Service
 public class PackageOrderService {
     @Autowired
     PackageOrderRepository packageOrderRepo;
+
+    @Autowired
+    UserRepository userRepo;
 
     public String createPackageOrder(PackageRequest packageRequest){
         PackageOrder packageOrder = new PackageOrder();
@@ -50,5 +58,13 @@ public class PackageOrderService {
         packageOrderRepo.save(order);
 
         return "order is canceled";
+    }
+
+    public List<PackageOrder> getSenderOrders(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String user_email = auth.getPrincipal().toString();
+        User user = userRepo.getUserByEmail(user_email);
+
+        return packageOrderRepo.getAllBySender(user);
     }
 }
