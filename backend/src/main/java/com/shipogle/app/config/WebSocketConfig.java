fdb1615c7d58a8d1aebe53e1fb6bbac4aacae27c
RHSaliya <RHS.Dev@outlook.com>
2023-03-16 22:35:37 -0300

    inbox: made chat screen userwise.

--- backend/src/main/java/com/shipogle/app/config/WebSocketConfig.java
+++ backend/src/main/java/com/shipogle/app/config/WebSocketConfig.java
@@ -1,21 +1,20 @@
 package com.shipogle.app.config;
 
-import org.springframework.boot.json.JsonParser;
-import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 @Configuration
@@ -30,7 +29,6 @@ public class WebSocketConfig implements WebSocketConfigurer {
                 .setHandshakeHandler(new DefaultHandshakeHandler());
     }
 
-
     @Bean
     public WebSocketHandler myHandler() {
         return new SocketHandler();
@@ -44,14 +42,10 @@ public class WebSocketConfig implements WebSocketConfigurer {
         public void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
             String payload = message.getPayload();
             System.out.println("Message received: " + payload);
-            // TODO: From here send message to other user
+
             session.sendMessage(new TextMessage(payload));
 
-            // TODO: Send message to other user
-            JsonParser jsonParser = JsonParserFactory.getJsonParser();
-            Map<String, Object> jsonMap = jsonParser.parseMap(payload);
-            String receiverId = jsonMap.get("receiverId").toString();
-            WebSocketSession receiverSession = sessions.get(receiverId);
+            WebSocketSession receiverSession = sessions.get(getUniqueID(session));
             if (receiverSession != null) {
                 receiverSession.sendMessage(new TextMessage(payload));
             }
@@ -59,11 +53,10 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
         @Override
         public void afterConnectionEstablished(WebSocketSession session) {
-            String url = Objects.requireNonNull(session.getUri()).toString();
-            String userId = url.substring(url.lastIndexOf("/") + 1);
-            sessions.put(userId, session);
+            sessions.put(getUniqueID(session), session);
         }
 
+        // TODO: make url senderId;receiverId, right now it is only senderId
         @Override
         public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
             String url = Objects.requireNonNull(session.getUri()).toString();
@@ -71,6 +64,11 @@ public class WebSocketConfig implements WebSocketConfigurer {
             WebSocketSession sessionOld = sessions.remove(userId);
             sessionOld.close();
             super.afterConnectionClosed(session, status);
+        }
+
+        private String getUniqueID(WebSocketSession session) {
+            String url = Objects.requireNonNull(session.getUri()).toString();
+            return url.substring(url.lastIndexOf("/") + 1);
         }
     }
 }
