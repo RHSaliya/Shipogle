6a69947e28ce600600216602ab8d8548673c6203
shahraj <sh880821@dal.ca>
2023-03-16 21:11:27 -0300

    etst

--- shipogle-frontend/src/App.js
+++ shipogle-frontend/src/App.js
@@ -1,15 +1,14 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Home from './pages/Home';
-import Registration from './pages/Registration';
-import RegistrationForm from './pages/RegistrationForm';
-import SendItems from './pages/SendItems';
-import Login from './pages/Login';
-import RegSuccessful from './pages/RegSuccessful';
-import UserDashboard from './pages/UserDashboard';
-import EditProfile from './pages/EditProfile';
-
+import Home from "./pages/Home";
+import Registration from "./pages/Registration";
+import RegistrationForm from "./pages/RegistrationForm";
+import SendItems from "./pages/SendItems";
 import Login from "./pages/Login";
+import RegSuccessful from "./pages/RegSuccessful";
+import UserDashboard from "./pages/UserDashboard";
+import EditProfile from "./pages/EditProfile";
+
 import CourierRegistration from "./pages/courierRegistration";
 import CourierListing from "./pages/couriersListing";
 import MapView from "./pages/mapView";
@@ -19,12 +18,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/registration" element={<Registration/>} />
-        <Route path="/registration/form" element={<RegistrationForm/>} />
-        <Route path="/registration/success" element={<RegSuccessful/>} />
-        <Route path="/userdash" element={<UserDashboard/>} />
-        <Route path="/user/editprofile" element = {<EditProfile />} />
-        <Route path="/userdash/send" element={<SendItems/>} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/registration/form" element={<RegistrationForm />} />
+        <Route path="/registration/success" element={<RegSuccessful />} />
+        <Route path="/userdash" element={<UserDashboard />} />
+        <Route path="/user/editprofile" element={<EditProfile />} />
+        <Route path="/userdash/send" element={<SendItems />} />
       </Routes>
     </div>
   );
