5b012e21948236b9bef8df09d449554a0916a1d3
RHSaliya <RHS.Dev@outlook.com>
2023-03-27 00:12:02 -0300

    notif_feature: Added notification clear api  call.

--- shipogle-frontend/src/components/NotificationsMenu.js
+++ shipogle-frontend/src/components/NotificationsMenu.js
@@ -10,7 +10,16 @@ import axios from "../utils/MyAxios";
 import Constants from "../Constants";
 import { w3cwebsocket as WebSocket } from "websocket";
 
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+
 export default function NotificationsMenu() {
+  const [dialogOpen, setDialogOpen] = React.useState(false);
+  const [user, setUser] = useState({});
   const [anchorEl, setAnchorEl] = useState(null);
   const [hasNotfication, setHasNotification] = useState(false);
   const [notifications, setNotifications] = useState([]);
@@ -26,23 +35,32 @@ export default function NotificationsMenu() {
     setAnchorEl(null);
   };
 
-  const addNotification = (notification) => {
-    setNotifications([...notifications, notification]);
-    setHasNotification(true);
-  }
+  const handleCloseDialog = () => {
+    setDialogOpen(false);
+  };
+
   const handleNotif = () => {
     setHasNotification(false);
   }
   const handleClearNotif = () => {
-    setClearNotif(prevClearNotif => true);
-
+    setDialogOpen(true);
   }
+
+  const handleClear = () => {
+    setDialogOpen(false);
+    axios.delete(`${Constants.API_NOTIFICATIONS}/all/${user.user_id}`).then((response) => {
+      console.log(response);
+      setClearNotif(prevClearNotif => true);
+    });
+  };
+
 
   useEffect(() => {
     // Get user info from token
     axios.get(Constants.API_USER_INFO_FROM_TOKEN).then((response) => {
       console.log("~~~~~~~~~~~~~~");
       const user = response.data;
+      setUser(user);
       console.log(user);
 
       // Get notifications for current user
@@ -79,6 +97,28 @@ export default function NotificationsMenu() {
 
   return (
     <div>
+      <Dialog
+        open={dialogOpen}
+        onClose={handleCloseDialog}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          Please confirm
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to clear all notifications?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog}>Cancel</Button>
+          <Button onClick={handleClear} autoFocus>
+            Clear
+          </Button>
+        </DialogActions>
+      </Dialog>
+
       <Button
         id="demo-positioned-button"
         aria-controls={open ? 'demo-positioned-menu' : undefined}
@@ -108,17 +148,17 @@ export default function NotificationsMenu() {
         }}
       >
         <div className="notification-header">
-        <p style={{ padding: "0 1em 0 1em" , fontSize: "20px", fontWeight: "bold", textAlign: "center"}}>Notifications</p>
-        <button className="btn" onClick={handleClearNotif}>Clear</button>
+          <p style={{ padding: "0 1em 0 1em", fontSize: "20px", fontWeight: "bold", textAlign: "center" }}>Notifications</p>
+          <button className="btn" onClick={handleClearNotif}>Clear</button>
 
         </div>
-       
+
         {
-         (clearNotif === true ?   <p style={{ padding: "0 1em 0 1em" ,textAlign: "center"}}>Empty Notifications</p> :
-         notifications.map((notification, index) => (
-          <MenuItem style={{borderBottom:"1px solid black"}} sx={{ width: "500px" }} onClick={handleClose}><Notification notificationName={notification.title} notificationAction={notification.message} /></MenuItem> )
-       
-        ))}
+          (clearNotif === true ? <p style={{ padding: "0 1em 0 1em", textAlign: "center" }}>Empty Notifications</p> :
+            notifications.map((notification, index) => (
+              <MenuItem style={{ borderBottom: "1px solid black" }} sx={{ width: "500px" }} onClick={handleClose}><Notification notificationName={notification.title} notificationAction={notification.message} /></MenuItem>)
+
+            ))}
       </Menu>
     </div>
   );
