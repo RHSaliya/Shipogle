4d852e9aeebe0b9bf53dcf318f8bdf75000732ce
RHSaliya <RHS.Dev@outlook.com>
2023-03-26 23:49:18 -0300

    frontend: Added update profile api call.

--- shipogle-frontend/src/Constants.js
+++ shipogle-frontend/src/Constants.js
@@ -4,6 +4,7 @@ const APIS = {
     API_REGISTER: `${BASE_URL}/register`,
     API_LOGIN: `${BASE_URL}/login`,
     API_CHAT: `${BASE_URL}/chat`,
+    API_USER: `${BASE_URL}/user`,
     API_USER_INFO_FROM_TOKEN: `${BASE_URL}/user_info`,
     API_NOTIFICATIONS: `${BASE_URL}/notifications`,
     API_FORGOT_PWD: `${BASE_URL}/forgotpassword`,
