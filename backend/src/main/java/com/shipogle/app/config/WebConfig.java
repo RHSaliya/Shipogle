5ee3f9e1278ca414107f69073124cc56056d9e5c
RHSaliya <RHS.Dev@outlook.com>
2023-03-29 20:39:36 -0300

    CICD_development: updated backend url.

--- backend/src/main/java/com/shipogle/app/config/WebConfig.java
+++ backend/src/main/java/com/shipogle/app/config/WebConfig.java
@@ -11,7 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");;
+        registry.addMapping("/**")
+                .allowedOrigins("http://csci5308vm9.research.cs.dal.ca")
+                .allowedOrigins("*");;
     }
 
 
