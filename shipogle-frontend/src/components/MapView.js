53253c9d5377f78008573f499bbcd4cd803edb2c
shahraj <sh880821@dal.ca>
2023-03-30 17:53:27 -0300

    changes in map view

--- shipogle-frontend/src/components/MapView.js
+++ shipogle-frontend/src/components/MapView.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 import CommonFunctions from "../services/CommonFunction";
 
 const containerStyle = {
@@ -16,21 +15,5 @@ export default function MapView(props) {
   const userLocation = commFunc.getCurrentLocation();
   const locations = [defaultCenter];
   console.log(props);
-  return (
-    <>
-      return (
-      <LoadScript googleMapsApiKey="YOUR_GOOGLE_MAPS_API_KEY">
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={defaultCenter}
-          zoom={12}
-        >
-          {locations.map((location, index) => (
-            <Marker key={index} position={location} />
-          ))}
-        </GoogleMap>
-      </LoadScript>
-      );
-    </>
-  );
+  return <>Testing</>;
 }
