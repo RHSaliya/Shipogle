4795f82a7a6a9f3d960aa92017df7bf2e1c264b1
RHSaliya <RHS.Dev@outlook.com>
2023-04-08 02:28:41 -0300

    TestCase: Cleaned tests from WebSocketConfigTest.java

--- backend/src/test/java/com/shipogle/app/config/WebSocketConfigTest.java
+++ backend/src/test/java/com/shipogle/app/config/WebSocketConfigTest.java
@@ -1,53 +1,22 @@
 package com.shipogle.app.config;
 
-import com.shipogle.app.model.Notification;
-import com.shipogle.app.model.User;
 import com.shipogle.app.socket_handlers.ChatSocketHandler;
 import com.shipogle.app.socket_handlers.NotificationSocketHandler;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistration;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
-import java.net.URI;
-import java.time.LocalDateTime;
-
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 @ExtendWith(MockitoExtension.class)
 public class WebSocketConfigTest {
-
-    @Test
-    public void testNotificationSocketHandler() throws Exception {
-        WebSocketSession session = Mockito.mock(WebSocketSession.class);
-        Mockito.when(session.getUri()).thenReturn(URI.create("ws://localhost:8080/notification/60"));
-
-        Notification notification = new Notification();
-        notification.setMessage("Hello, world!");
-        notification.setTitle("Test");
-        notification.setPayload("{}");
-        notification.setType("test");
-        notification.setCreatedAt(LocalDateTime.now());
-        User user = new User();
-        user.setUser_Id(60);
-        notification.setUser(user);
-
-        String expectedPayload = notification.toString();
-
-        NotificationSocketHandler handler = NotificationSocketHandler.getInstance();
-        handler.afterConnectionEstablished(session);
-        handler.sendNotification(notification);
-
-        Mockito.verify(session, Mockito.times(1)).sendMessage(new TextMessage(expectedPayload));
-    }
 
     @Test
     public void testWebSocketOrigins() {
