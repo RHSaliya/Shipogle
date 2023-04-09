9c2780a04c7ec7e8f12fb5f2dd73919b2829d0d5
shahraj <sh880821@dal.ca>
2023-04-08 22:27:41 -0300

    bug fixes in start delivery

--- shipogle-frontend/src/pages/StartEndDelivery.js
+++ shipogle-frontend/src/pages/StartEndDelivery.js
@@ -1,42 +1,71 @@
-import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import React, { useState, useEffect, useCallback } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import Constants from "../Constants";
 import customAxios from "../utils/MyAxios";
-import Data from "./data";
 import { Button, Card, CircularProgress, TextField } from "@mui/material";
 import CommonFunctions from "../services/CommonFunction";
 
 const StartEndDelivery = () => {
-  const commFunc = new CommonFunctions();
+  const commFunc = useCallback(() => new CommonFunctions(), []);
   const [isLoading, setIsLoading] = useState(true);
   const { state } = useLocation();
   const [orders, setOrders] = useState([]);
+  const [canceledOrders, setCanceledOrders] = useState([]);
   const [codes, setCodes] = useState("");
   const routeId = state.id;
+  const navigate = useNavigate();
 
-  const fetchOrders = () => {
+  const sendNotification = useCallback(
+    (body) => {
+      customAxios.post(Constants.API_NOTIFICATIONS, body).then(
+        (res) => {},
+        (error) => {
+          console.error(error);
+          commFunc.showAlertMessage(
+            "Request sent but failed to send driver a Notification!",
+            "error",
+            3000,
+            "bottom"
+          );
+        }
+      );
+    },
+    [commFunc]
+  );
+
+  const createNotificationBodyForNonCancelledOrders = useCallback(
+    (orders) => {
+      orders.forEach((order) => {
+        let body = {
+          userId: order.sender.user_id,
+        };
+        if (!order.started) {
+          body["title"] = "Pickup Code";
+          body[
+            "message"
+          ] = `Please provide pickup code ${order.pickup_code} to ${order.driverRoute.driverName} to start delivery`;
+        } else if (order.started && !order.delivered) {
+          body["title"] = "Drop Code";
+          body[
+            "message"
+          ] = `Please provide pickup code ${order.drop_code} to ${order.driverRoute.driverName} to end delivery`;
+        }
+        sendNotification(body);
+      });
+    },
+    [sendNotification]
+  );
+
+  const fetchOrders = useCallback(() => {
     customAxios(
       Constants.GETALLORDERSFROMDRIVERROUTEID + "?driver_route_id=" + routeId
     ).then(
       (res) => {
-        setOrders(res.data);
-        res.data.forEach((order) => {
-          let body = {
-            userId: order.sender.user_id,
-          };
-          if (!order.started && !order.canceled) {
-            body["title"] = "Pickup Code";
-            body[
-              "message"
-            ] = `Please provide pickup code ${order.pickup_code} to ${order.driverRoute.driverName} to start delivery`;
-          } else if (order.started && !order.delivered && !order.canceled) {
-            body["title"] = "Drop Code";
-            body[
-              "message"
-            ] = `Please provide pickup code ${order.drop_code} to ${order.driverRoute.driverName} to end delivery`;
-          }
-          sendNotification(body);
-        });
+        const nonCanceledOrders = res.data.filter((order) => !order.canceled);
+        const canceledOrders = res.data.filter((order) => order.canceled);
+        setOrders(nonCanceledOrders);
+        setCanceledOrders(canceledOrders);
+        createNotificationBodyForNonCancelledOrders(nonCanceledOrders);
         setIsLoading(false);
       },
       (error) => {
@@ -49,7 +78,7 @@ const StartEndDelivery = () => {
         console.error(error);
       }
     );
-  };
+  }, [commFunc, createNotificationBodyForNonCancelledOrders, routeId]);
 
   const submit = (e) => {
     e.preventDefault();
@@ -59,6 +88,7 @@ const StartEndDelivery = () => {
       orders[0].started && !orders[0].canceled
         ? Constants.ENDORDER
         : Constants.STARTORDER;
+    const requests = [];
     for (let a = 0; a < code.length; a++) {
       let body = {
         order_id: orders[a].id,
@@ -66,20 +96,10 @@ const StartEndDelivery = () => {
       const codeString =
         orders[0].started && !orders[0].canceled ? "drop_code" : "pickup_code";
       body[codeString] = parseInt(code[a]);
-      customAxios.put(api, body).then(
+      const request = customAxios.put(api, body).then(
         (res) => {
           commFunc.showAlertMessage("code success", "success", 1000, "bottom");
-          const body = {
-            userId: orders[a].sender.user_id,
-            title:
-              codeString === "drop_code"
-                ? "delivery ended"
-                : "delivery started",
-            message:
-              codeString === "drop_code"
-                ? `${orders[a].driverRoute.driverName} has delivered your courier`
-                : `${orders[a].driverRoute.driverName} has picked up your courier`,
-          };
+          createBodyNotificationBodyForActionOnOrder(orders[a]);
           sendNotification(body);
         },
         (error) => {
@@ -92,27 +112,42 @@ const StartEndDelivery = () => {
           );
         }
       );
+      requests.push(request);
+    }
+    waitAndRouteToCurrentDeliveries(requests);
+  };
+
+  const waitAndRouteToCurrentDeliveries = async (promises) => {
+    try {
+      await Promise.all(promises);
+    } catch (error) {
+      commFunc.showAlertMessage(
+        "an error occured while verifying codes",
+        "error",
+        3000,
+        "bottom"
+      );
+    } finally {
+      navigate("/deliveries");
     }
   };
 
-  const sendNotification = (body) => {
-    customAxios.post(Constants.API_NOTIFICATIONS, body).then(
-      (res) => {},
-      (error) => {
-        console.error(error);
-        commFunc.showAlertMessage(
-          "Request sent but failed to send driver a Notification!",
-          "error",
-          3000,
-          "bottom"
-        );
-      }
-    );
+  const createBodyNotificationBodyForActionOnOrder = (order) => {
+    let codeString;
+    const body = {
+      userId: order.sender.user_id,
+      title: codeString === "drop_code" ? "delivery ended" : "delivery started",
+      message:
+        codeString === "drop_code"
+          ? `${order.driverRoute.driverName} has delivered your courier`
+          : `${order.driverRoute.driverName} has picked up your courier`,
+    };
+    return body;
   };
 
   useEffect(() => {
     fetchOrders();
-  }, []);
+  }, [fetchOrders]);
 
   return (
     <Card style={{ maxWidth: "400px", margin: "2rem auto", padding: "1rem" }}>
