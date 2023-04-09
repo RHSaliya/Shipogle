1c2ba9b9b7575c37494808cc5331c37938d241bc
RHSaliya <RHS.Dev@outlook.com>
2023-04-09 13:56:58 -0300

    refactor: made changes in PaymentControllerIntegrationTest.java

--- backend/src/test/java/com/shipogle/app/controller/PaymentControllerIntegrationTest.java
+++ backend/src/test/java/com/shipogle/app/controller/PaymentControllerIntegrationTest.java
@@ -1,51 +1,47 @@
-//package com.shipogle.app.controller;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.shipogle.app.model.PaymentGatewayRequest;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.MvcResult;
-//
-//import java.math.BigDecimal;
-//
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//public class PaymentControllerIntegrationTest {
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    private String token = "eyJhbGciOiJIUzM4NCJ9.eyJlbWFpbCI6InF1cGlmdWdpdG9AZ290Z2VsLm9yZyIsInN1YiI6IlNoaXZhbSIsImlhdCI6MTY4MDkzNzY0OCwiZXhwIjoxNjgxMDI0MDQ4fQ.gHGmWb-3bFScWgq5Z_zizfCPd-FwrvBI6qTvDSQsQ1CmwNjJd1AbnMh_ZAP-0Nbl";
-//
-//    @Test
-//    public void testChargeCreditCard() throws Exception {
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        PaymentGatewayRequest paymentGatewayRequest = new PaymentGatewayRequest();
-//        PaymentGatewayRequest paymentRequest = new PaymentGatewayRequest();
-//        paymentRequest.setAmount(new BigDecimal("189.00"));
-//        paymentRequest.setCurrency("USD");
-//        paymentRequest.setCardNumber("4112323111111111");
-//        paymentRequest.setCardExpiryMonth(12);
-//        paymentRequest.setCardExpiryYear(2025);
-//        paymentRequest.setCardCvv("567");
-//        paymentRequest.setCardHolderName("Stuart Clark");
-//
-//        String jsonString = objectMapper.writeValueAsString(paymentGatewayRequest);
-//
-//        MvcResult result = mockMvc.perform(post("/payment/charge")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .header("Authorization",
-//                                "Bearer " + token + "\"")
-//                        .content(jsonString))
-//                .andExpect(status().isOk())
-//                .andReturn();
-//    }
-//}
-//
+package com.shipogle.app.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.shipogle.app.model.PaymentGatewayRequest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.math.BigDecimal;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
+public class PaymentControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    public void testChargeCreditCard() throws Exception {
+        String endpoint = "/payment/charge";
+        PaymentGatewayRequest paymentRequest = new PaymentGatewayRequest();
+        paymentRequest.setAmount(new BigDecimal("189.00"));
+        paymentRequest.setCurrency("USD");
+        paymentRequest.setCardNumber("4112323111111111");
+        paymentRequest.setCardExpiryMonth(12);
+        paymentRequest.setCardExpiryYear(2025);
+        paymentRequest.setCardCvv("567");
+        paymentRequest.setCardHolderName("Stuart Clark");
+        System.out.println(objectMapper.writeValueAsString(paymentRequest));
+
+        mockMvc.perform(MockMvcRequestBuilders.post(endpoint)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization","Bearer eyJhbGciOiJIUzM4NCJ9.eyJlbWFpbCI6InNoaXBvZ2xlLnRlc3R1c2VyMUB5b3BtYWlsLmNvbSIsInN1YiI6IlRlc3QiLCJpYXQiOjE2ODA5OTAwNjN9.b4DlK4cXAOzYAnZsFl5xAFFvIMJrv85QyMYtf-koS_Jq4h4UA6BHlDc1fmrdaZ9P")
+                        .content(objectMapper.writeValueAsString(paymentRequest)))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
