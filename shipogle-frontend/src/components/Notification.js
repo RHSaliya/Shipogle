0d9d324959ab8ebad1f84245d7f23b06cec75397
shahraj <sh880821@dal.ca>
2023-04-09 04:06:42 -0300

    fix in notification

--- shipogle-frontend/src/components/Notification.js
+++ shipogle-frontend/src/components/Notification.js
@@ -1,28 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
-// notifType 0 -> You have a chat message
-// notifType 1 -> You have a match (either with a sender or deliverer).
-function getRoute(notifType) {
-  if (notifType === 0) {
-    return "/inbox";
-  } else {
-    return "/";
-  }
-}
-
 export default function Notification(props) {
   const [notifType, setNotifType] = React.useState(0);
-
-  const route = getRoute(notifType);
+  // notifType 0 -> You have a chat message
+  // notifType 1 -> You have a match (either with a sender or deliverer).
 
   return (
     <div className="notif">
       <h4> {props.notificationName}</h4>
-      <Link to={route}>
-        <p> {props.notificationAction}</p>
-      </Link>
+
+      {notifType === 0 ? (
+        <Link to="/inbox">
+          {" "}
+          <p> {props.notificationAction}</p>
+        </Link>
+      ) : (
+        <Link to="/">
+          {" "}
+          <p> {props.notificationAction}</p>
+        </Link>
+      )}
     </div>
   );
 }
